e3512c5f5c19ef120df603ec58e2fd1b06f00424
chetna14manku <rchetna101@gmail.com>
2021-12-08 13:47:13 +0530

    check split str

--- src/Helper.groovy
+++ src/Helper.groovy
@@ -7,7 +7,7 @@ def printString(String Name) {
 
 def createEnv() {
     String test = "https://gitlab.redhat.com/cchetna/mock_rhel.git@master"
-    String str = String.join("envs", "${test}".split("/")[-1].split("git")[0])
+    String str = String.join("/", "envs", "${test}".split("/")[-1].split(".git")[0])
     echo ("Split str is '${str}' ")
 }
 
