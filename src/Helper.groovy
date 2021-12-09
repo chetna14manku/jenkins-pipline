999cf5e3f90292324784b24d79c8064a37c95eba
chetna14manku <rchetna101@gmail.com>
2021-12-09 14:57:07 +0530

    change class

--- src/Helper.groovy
+++ src/Helper.groovy
@@ -1,15 +1,20 @@
 import groovy.transform.Field
 
-def file = load '../vars/demo.sh' 
+class Helper {
+    
+    String p = "${params.PACKAGE}"
 
 def printString(String Name) {
       echo ("Create String '${Name}' ")
+      echo ("Create String '${p}' ")
 }
 
 def createEnv() {
     String test = "https://gitlab.redhat.com/cchetna/mock_rhel.git@master"
     String str = String.join("/", "envs", "${test}".split("/")[-1].split(".git")[0])
     echo ("Split str is '${str}' ")
+    echo ("Create String '${p}' ")
+
 }
 
 def cpFile() {
@@ -17,5 +22,6 @@ def cpFile() {
     cp demo.sh demo.sh
     ''')
 }
+}
 
-return this
+return new Helper()
