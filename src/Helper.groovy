6ba17de0f2ca0f722786698c365d5e743c627489
Chetna <69139323+chetna14manku@users.noreply.github.com>
2021-12-09 16:49:03 +0530

    Update Helper.groovy

--- src/Helper.groovy
+++ src/Helper.groovy
@@ -1,7 +1,7 @@
 import groovy.transform.Field
 
 
-String p
+String p = "${params.PACKAGE}"
     //def file = load 'vars/demo.sh' 
 
     def printString(String Name) {
@@ -10,7 +10,7 @@ String p
     }
 
     def createEnv() {
-        p = getPackage()
+       // p = getPackage()
         String test = "https://gitlab.redhat.com/cchetna/mock_rhel.git@master"
         String str = String.join("/", "envs", "${test}".split("/")[-1].split(".git")[0])
         echo ("Split str is '${str}' ")
