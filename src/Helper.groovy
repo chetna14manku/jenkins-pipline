8e6aa558571dd66c4b2f7e0563d9281899124e15
Chetna <69139323+chetna14manku@users.noreply.github.com>
2021-12-09 18:08:18 +0530

    Update Helper.groovy

--- src/Helper.groovy
+++ src/Helper.groovy
@@ -16,20 +16,21 @@ def setParams() {
     //def file = load 'vars/demo.sh' 
 
     def printString(String Name) {
+      setParams() 
       echo ("Create String '${Name}' ")
       echo ("Create String '${p}' ")
     }
 
     def createEnv() {
         echo ("Setting params----")
-        setParams() 
+        
        // p = getPackage()
        // String test = "https://gitlab.redhat.com/cchetna/mock_rhel.git@master"
        // String str = String.join("/", "envs", "${test}".split("/")[-1].split(".git")[0])
         echo ("Split str is '${str}' ")
         echo ("Create String '${p}' ")
         echo ("WORKSPACE '${w}' ")
-        echo ("VENV: ${venvDir}")
+       // echo ("VENV: ${venvDir}")
     }
 
     def cpFile(String venv) {
