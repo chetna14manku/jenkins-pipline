09daabdb5430347191523ab936cf38c58283e38d
Chetna <69139323+chetna14manku@users.noreply.github.com>
2021-12-09 15:03:10 +0530

    Update Helper.groovy

--- src/Helper.groovy
+++ src/Helper.groovy
@@ -1,26 +1,23 @@
 import groovy.transform.Field
 
-<<<<<<< HEAD
 class Helper {
     
     String p = "${params.PACKAGE}"
-=======
-//def file = load 'vars/demo.sh' 
->>>>>>> 31e5afd351ef2b53b2babecddbbad3310c9205ab
+    //def file = load 'vars/demo.sh' 
 
-def printString(String Name) {
+    def printString(String Name) {
       echo ("Create String '${Name}' ")
       echo ("Create String '${p}' ")
-}
+    }
 
-def createEnv() {
-    String test = "https://gitlab.redhat.com/cchetna/mock_rhel.git@master"
-    String str = String.join("/", "envs", "${test}".split("/")[-1].split(".git")[0])
-    echo ("Split str is '${str}' ")
-    echo ("Create String '${p}' ")
+    def createEnv() {
+        String test = "https://gitlab.redhat.com/cchetna/mock_rhel.git@master"
+        String str = String.join("/", "envs", "${test}".split("/")[-1].split(".git")[0])
+        echo ("Split str is '${str}' ")
+        echo ("Create String '${p}' ")
+    }
 
-}
-
-def cpFile() {
-    echo ("try Copy")
-}
+    def cpFile() {
+        echo ("try Copy")
+    }
+}    
